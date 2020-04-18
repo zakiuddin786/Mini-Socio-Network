cfe9709f10f81b1eb22b774957cb38ae8038c86a
zakiuddin786 <zakiuddin321@gmail.com>
2020-04-18 16:49:10 +0530

    hope this works

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/network'));
+app.use(express.static(__dirname+'./dist/network'));
 
-app.get('/*', function(req,res) {
+app.get('/', function(req,res) {
     
 res.sendFile(path.join(__dirname, 'dist', 'network', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000
 
-server.listen(PORT, () => console.log(`socio network listening on port ${PORT}!`));
+app.listen(PORT, () => console.log(`socio network listening on port ${PORT}!`));
